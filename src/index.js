0c8b94184338026e91873f8b5768c56b0afc8e4d
Alexander Tolchinsky <bombur@kaap.io>
2020-10-26 12:35:19 +0300

    add pug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import fastify from 'fastify';
+import pointOfView from 'point-of-view';
+import pug from 'pug';
+import { fileURLToPath } from 'url';
+import { resolve, dirname } from 'path';
+
+/* eslint-disable no-underscore-dangle */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const setUpViews = (app) => {
+  app.register(pointOfView, {
+    engine: {
+      pug,
+    },
+    root: resolve(__dirname, '..', 'src', 'views'),
+    includeViewExtension: true,
+  });
+};
 
 const application = fastify();
+setUpViews(application);
 
 export default application;
